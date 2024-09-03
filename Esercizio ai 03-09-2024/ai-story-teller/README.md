2ef445d794a5aa7d4aa05b17a0dce7fd2f34d25e
Birdofillome <chiara.corvitto95@gmail.com>
2024-09-03 20:58:08 +0200

    Esercizio 03-09-2024

---
+++ Esercizio ai 03-09-2024/ai-story-teller/README.md
@@ -0,0 +1,19 @@
+# titolo h1
+
+## titolo h2
+
+### titolo h3
+
+#### titolo h4
+
+##### titolo h5
+
+###### titolo h6
+
+V metodi mixin sass
+
+- inserire campi input -> atom
+- inserire select -> atom
+- inserire switch (per definire bambino/adulto e condizionare l'output) -> atom
+- hamburger menù
+- carousel
---
+++ Esercizio ai 03-09-2024/ai-story-teller/src/constants/common.ts
@@ -0,0 +1,22 @@
+export const ListaGeneri = [
+    {
+      label: "Horror",
+      value: "Horror"
+    },
+    {
+      label: "Fantasy",
+      value: "Fantasy"
+    },
+    {
+      label: "Romantico",
+      value: "Romantico"
+    },
+    {
+      label: "Fantascienza",
+      value: "Fantascienza"
+    },
+    {
+      label: "Giallo",
+      value: "Giallo"
+    },
+  ]
